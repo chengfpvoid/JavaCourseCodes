990b0b446694e5dfa42ae4e2cb0b0738331da006
chengfpvoid <cfpvoid@gmail.com>
2020-11-11 22:09:20 +0800

    第四周作业2：使用循环阻塞主线程直到拿到结果

--- 03concurrency/0301/src/main/java/java0/conc0303/Homework03.java
+++ 03concurrency/0301/src/main/java/java0/conc0303/Homework03.java
@@ -1,6 +1,6 @@
 package java0.conc0303;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * 本周作业：（必做）思考有多少种方式，在main函数启动一个新线程或线程池，
@@ -27,12 +27,17 @@ public class Homework03 {
         // });
         // executorService.shutdown();
         // countDownLatch.await();
-        int result = CompletableFuture.supplyAsync(()-> sum()).join();
+        //int result = CompletableFuture.supplyAsync(()-> sum()).join();
 
         //int result = sum(); //这是得到的返回值
         //int result = future.get();
+        AtomicReference<Integer> result = new AtomicReference<>();
+        new Thread(() -> result.set(sum())).start();
         // 确保  拿到result 并输出
-        System.out.println("异步计算结果为："+result);
+        while (result.get() == null) {
+
+        }
+        System.out.println("异步计算结果为："+result.get());
 
         System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
 
