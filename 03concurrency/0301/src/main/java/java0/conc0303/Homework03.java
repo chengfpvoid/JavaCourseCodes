2284d3282b6f76783aa54fed29bf088fc4528271
chengfpvoid <cfpvoid@gmail.com>
2020-11-11 10:35:39 +0800

    第四周作业2：方法一 使用future#get拿到异步任务的返回值

--- 03concurrency/0301/src/main/java/java0/conc0303/Homework03.java
+++ 03concurrency/0301/src/main/java/java0/conc0303/Homework03.java
@@ -1,5 +1,9 @@
 package java0.conc0303;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 /**
  * 本周作业：（必做）思考有多少种方式，在main函数启动一个新线程或线程池，
  * 异步运行一个方法，拿到这个方法的返回值后，退出主线程？
@@ -9,14 +13,17 @@ package java0.conc0303;
  */
 public class Homework03 {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         
         long start=System.currentTimeMillis();
         // 在这里创建一个线程或线程池，
         // 异步执行 下面方法
-        
-        int result = sum(); //这是得到的返回值
-        
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Future<Integer> future = executorService.submit(() -> sum());
+        executorService.shutdown();
+
+        //int result = sum(); //这是得到的返回值
+        int result = future.get();
         // 确保  拿到result 并输出
         System.out.println("异步计算结果为："+result);
          
