31c22d06084a7c011db42f131a79d0bab8e751c4
chengfpvoid <cfpvoid@gmail.com>
2020-11-11 13:47:52 +0800

    第四周作业2：使用CountDownLatch方法

--- 03concurrency/0301/src/main/java/java0/conc0303/Homework03.java
+++ 03concurrency/0301/src/main/java/java0/conc0303/Homework03.java
@@ -1,8 +1,8 @@
 package java0.conc0303;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 /**
  * 本周作业：（必做）思考有多少种方式，在main函数启动一个新线程或线程池，
@@ -18,14 +18,21 @@ public class Homework03 {
         long start=System.currentTimeMillis();
         // 在这里创建一个线程或线程池，
         // 异步执行 下面方法
+        CountDownLatch countDownLatch = new CountDownLatch(1);
         ExecutorService executorService = Executors.newCachedThreadPool();
-        Future<Integer> future = executorService.submit(() -> sum());
-        executorService.shutdown();
 
+       // Future<Integer> future = executorService.submit(() -> sum());
+        executorService.execute( () -> {
+           int result = sum();
+            System.out.println("异步计算结果为："+result);
+            countDownLatch.countDown();
+        });
+        executorService.shutdown();
+        countDownLatch.await();
         //int result = sum(); //这是得到的返回值
-        int result = future.get();
+        //int result = future.get();
         // 确保  拿到result 并输出
-        System.out.println("异步计算结果为："+result);
+        //System.out.println("异步计算结果为："+result);
          
         System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
         
